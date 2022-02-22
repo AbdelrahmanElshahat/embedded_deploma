f602b0c0d981986f15b1ea60df8cddc2922ce00a
AbdelrahmanElshahat <abdelrahmanelshahat00@gmail.com>
2022-02-23 01:37:42 +0200

    change floder name

---
+++ c_basics/Ex4/Ex4.c
@@ -0,0 +1,11 @@
+//
+// Created by abdelrahmanelshahat on 23/02/2022.
+//
+#include <stdio.h>
+int main(){
+    float x,y;
+    printf("Enter two numbers:");
+    scanf("%f %f",&x,&y);
+    printf("product =%f",x*y);
+    return 0;
+}
