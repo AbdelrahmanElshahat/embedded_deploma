0522c5a15d86429d16c3d64fd97af086ef2cd25e
AbdelrahmanElshahat <abdelrahmanelshahat00@gmail.com>
2022-02-23 01:20:29 +0200

    solution of homework1

---
+++ Homework 1/Ex3/Ex3.c
@@ -0,0 +1,12 @@
+//
+// Created by abdelrahmanelshahat on 23/02/2022.
+//
+#include <stdio.h>
+int main(){
+    int x,y;
+    printf("Enter two integers:");
+    scanf("%d %d",&x ,&y);
+    printf("Sum =%d",x+y);
+
+    return 0;
+}
---
+++ Homework 1/Ex5/Ex5.c
@@ -0,0 +1,11 @@
+//
+// Created by abdelrahmanelshahat on 23/02/2022.
+//
+#include<stdio.h>
+int main(){
+    char x;
+    printf("Enter Character:");
+    scanf("%c",&x);
+    printf("ASCII value of %c = %d",x,x);
+    return 0;
+}
